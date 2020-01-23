de118a4ce48c5a419ab8916b5af14eab9cf20aa4
fetails <45269692+fetails@users.noreply.github.com>
2020-01-23 23:42:58 +0100

    Reworking

--- shaders/fragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 300 es
-precision mediump float;
-
-out vec4 outColor;  // you can pick any name\n' +
-in vec3 send_coords;
-
-void main() {
-    outColor = vec4(send_coords.x + 0.25, send_coords.y + 0.25, 0.75, 1);
-}
--- shaders/vertex.glsl
+++
@@ -1,8 +0,0 @@
-#version 300 es
-in vec3 coords;
-out vec3 send_coords;
-
-void main() {
-  gl_Position = vec4(coords, 1.0);
-  send_coords = coords;
-}
