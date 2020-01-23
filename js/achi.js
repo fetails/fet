032319db28aa2dd55693a4a132827d96b8eefb1c
fetails <45269692+fetails@users.noreply.github.com>
2020-01-23 18:31:57 +0100

    upd

--- js/achi.js
+++ js/achi.js
@@ -1,5 +1,9 @@
 class AchiConfig
 {
+    /**
+     * Constructor of AchiConfig.
+     * Store all of you're values in here.
+     */
     constructor()
     {
         this.message = "achi v0.2";
@@ -7,11 +11,17 @@ class AchiConfig
         this.showLines = false;
         this.animate = false;
         this.alphaModifier = 0.50;
+        this.zoom = 25;
     }
 }
 
 class AchiVertex
 {
+   /**
+    * 
+    * @param {Array} buffer - Set's VBO Data
+    * @param {Array} indices - Set's IBO Data
+    */
     constructor(buffer, indices)
     {
         this.vertex = buffer;
@@ -19,8 +29,30 @@ class AchiVertex
     }
 }
 
+class AchiColor
+{
+    /**
+     * 
+     * @param {number} red - Color RED
+     * @param {number} green - Color GREEN
+     * @param {number} blue - Color BLUE
+     * @param {number} alpha - Color ALPHA
+     */
+    constructor( red, green, blue, alpha )
+    {
+        this.r = red / 255;
+        this.g = green / 255;
+        this.b = blue / 255;
+        this.a = alpha / 255;
+    }
+}
+
 class AchiShaderExperimental
 {
+    /**
+     * 
+     * @param {Array} vertex - This is an experimental shader structure. Do not use.
+     */
     constructor(vertex)
     {
         let gl = ogl;
@@ -37,6 +69,11 @@ class AchiShaderExperimental
 
 class AchiShader
 {
+    /**
+     * 
+     * @param {string} vertex - Vertex Shader Source
+     * @param {string} fragment - Fragment Shader Source
+     */
     constructor(vertex, fragment)
     {
         this.gl = ogl;
@@ -56,21 +93,37 @@ class AchiShader
         gl.deleteShader(fs);
         this.program = prog;
     }
+    /**
+     * 
+     * @param {string} name - Target location in shader.
+     * @param {number} val - Value to modify.
+     */
     uniform_float(name, val)
     {
         let gl = this.gl;
         gl.uniform1f(gl.getUniformLocation(this.program, name), val);
     }
+    /**
+     * 
+     * @param {string} name - Target location in shader.
+     * @param {number} val - Value to modify.
+     */
     uniform_matrix4f(name, val)
     {
         let gl = this.gl;
         gl.uniformMatrix4fv(gl.getUniformLocation(this.program, name), gl.FALSE, val);
     }
+    /**
+     * Call this to bind our shader.
+     */
     bind()
     {
         let gl = this.gl;
         gl.useProgram(this.program);
     }
+    /**
+     * Call this to unbind our shader.
+     */
     unbind()
     {
         let gl = this.gl;
@@ -80,18 +133,29 @@ class AchiShader
 
 class Achi
 {
+    /**
+     * 
+     * @param {Any} gl - WebGL Context.
+     */
     constructor(gl)
     {
         this.gl = gl;
         this.options = new AchiConfig();
     }
+    /**
+     * Render Dat GUI.
+     */
     gui()
     {
         let gui = new dat.GUI();
         gui.add(this.options, "message");
         gui.add(this.options, "showLines");
         gui.addColor(this.options, "clearCol");
+        gui.add(this.options, "zoom");
     }
+    /**
+     * Tell's WebGL to clear our screen.
+     */
     clear()
     {
         stats.begin();
@@ -102,15 +166,23 @@ class Achi
         gl.viewport(0, 0, canvas.width, canvas.height);
         gl.enable(gl.DEPTH_TEST);
     }
+    /**
+     * 
+     * @param {AchiVertex} buffer - Add's a new AchiVertex buffer.
+     */
     add(buffer)
     {
         let gl = this.gl;
-        this.buffer = buffer.vertex;
         this.indices = buffer.indices;
-        this.indsize = buffer.indices.length;
-
+        this.buffer = buffer.vertex;
+    }
+    /**
+     * Create's VBO & IBO Data, ready to Render.
+     */
+    create()
+    {
+        let gl = this.gl;
         this.vbo = gl.createBuffer();
-        
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.buffer), gl.STATIC_DRAW);
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
@@ -119,18 +191,109 @@ class Achi
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ibo);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.indices), gl.STATIC_DRAW);
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
-        console.log("[WebGL] Indices:", this.indsize);
     }
+    /**
+     * Render's an example rectangle.
+     * @param {number} x - X Position of quad
+     * @param {number} y - Y Position of quad
+     * @param {number} w - W Width of quad
+     * @param {number} h - H Height of quad
+     */
+    quad(x, y, w, h)
+    {
+        this.add(new AchiVertex([
+            x, y + h, 0.0,
+            x + w, y + h, 0.0,
+            x + w, y, 0.0,
+            x, y, 0.0
+        ], [ 
+            0, 1, 2,
+            3, 0, 2
+        ]));
+    }
+    /**
+     * Render's an example 3D rectangle.
+     */
+    cube3D()
+    {
+        this.add(new AchiVertex(
+            [
+                -1.0, 1.0, -1.0,   
+                -1.0, 1.0, 1.0,    
+                1.0, 1.0, 1.0,     
+                1.0, 1.0, -1.0,    
+        
+                // Left
+                -1.0, 1.0, 1.0,    
+                -1.0, -1.0, 1.0,   
+                -1.0, -1.0, -1.0,  
+                -1.0, 1.0, -1.0,   
+        
+                // Right
+                1.0, 1.0, 1.0,    
+                1.0, -1.0, 1.0,   
+                1.0, -1.0, -1.0,  
+                1.0, 1.0, -1.0,   
+        
+                // Front
+                1.0, 1.0, 1.0,    
+                1.0, -1.0, 1.0,   
+                -1.0, -1.0, 1.0,   
+                -1.0, 1.0, 1.0,    
+        
+                // Back
+                1.0, 1.0, -1.0,    
+                1.0, -1.0, -1.0,   
+                -1.0, -1.0, -1.0,  
+                -1.0, 1.0, -1.0,   
+        
+                // Bottom
+                -1.0, -1.0, -1.0,  
+                -1.0, -1.0, 1.0,   
+                1.0, -1.0, 1.0,    
+                1.0, -1.0, -1.0
+                ], [
+                0, 1, 2,
+                0, 2, 3,
+        
+                // Left
+                5, 4, 6,
+                6, 4, 7,
+        
+                // Right
+                8, 9, 10,
+                8, 10, 11,
+        
+                // Front
+                13, 12, 14,
+                15, 14, 12,
+        
+                // Back
+                16, 17, 18,
+                16, 18, 19,
+        
+                // Bottom
+                21, 20, 22,
+                22, 20, 23
+                ]
+        ));
+    }
+    /**
+     * Begin's our rendering process.
+     */
     begin()
     {
         let gl = this.gl;
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ibo);
 
+        // vec3 -> 3.
         gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 0, 0);
         gl.enableVertexAttribArray(0);
     }
+    /**
+     * End's our rendering process.
+     */
     end()
     {
         let gl = this.gl;
@@ -138,17 +301,24 @@ class Achi
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
     }
-    render()
+    /**
+     * Tell's WebGL to render all of our elements that we've pushed.
+     */
+    render(len = this.indices.length)
     {
         let gl = this.gl;
         if(this.options.showLines)
-            gl.drawElements(gl.LINE_STRIP, this.indices.length, gl.UNSIGNED_SHORT, 0);
+            gl.drawElements(gl.LINE_LOOP, len, gl.UNSIGNED_SHORT, 0);
         else
-            gl.drawElements(gl.TRIANGLES, this.indices.length, gl.UNSIGNED_SHORT, 0);
+            gl.drawElements(gl.TRIANGLES, len, gl.UNSIGNED_SHORT, 0);
     }
+    /**
+     * Delete's the VBO & IBO buffer.
+     */
     del()
     {
         let gl = this.gl;
         gl.deleteBuffer(this.vbo);
+        gl.deleteBuffer(this.ibo);
     }
 };
